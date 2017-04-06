0afac787d098abec3b797a8b9558880f98b54048
Jonathan Finch <jfinch@rawnet.com>
2017-04-06 13:27:16 +0100

    Enhanced

--- TwitterCompetitionWinner.php
+++ TwitterCompetitionWinner.php
@@ -8,31 +8,34 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
+$winningStrings = ['Winning String1 ', 'Winning string2'];
+$q = '@rawnet';
 
-//$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+$statuses = $connection->get("search/tweets", ["count" => 100,"q" => $q]);
 
-$hashtag = '#rawnet';
-$winningString = 'symfony';
-
-$statuses = $connection->get("search/tweets", ["count" => 100,"q" => $hashtag]);
-var_dump($statuses);
-
-// TODO check total counter
 $tweets = $statuses->statuses;
 
 $potentialWinners = [];
 foreach ($tweets as $tweet) {
-	
-	if (strpos($tweet->text, $winningString) !== false) {
-		
-		
-		$potentialWinners[] = $tweet->user->name;
-		$potentiallyWinningTweet[$tweet->user->name][] = $tweet;
-	}
-	
+
+    foreach ($winningStrings as $winningString) {
+        if (strpos($tweet->text, $winningString) !== false) {
+            $potentialWinners[] = $tweet->user->name;
+            $potentiallyWinningTweet[$tweet->user->name][] = $tweet;
+            break;
+        }
+    }
 }
 
 $potentialWinners = array_unique($potentialWinners);
+echo <<<EOL
+RAWNET PHP TWITTER TAKEOVER COMPETITION WINNER SELECTOR
+
+EOL;
+if (empty($potentialWinners)) {
+    echo "No Winners!";
+    exit(0);
+}
 $winner = $potentialWinners[mt_rand(0, count($potentialWinners) - 1)];
 echo "The winner is $winner".PHP_EOL;
 $winningTweet = $potentiallyWinningTweet[$winner][mt_rand(0, count($potentiallyWinningTweet[$winner]) - 1)]->text;
