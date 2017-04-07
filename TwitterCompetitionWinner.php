1286e23a8a388334f9373fbb5219e732249d0d1e
Jonathan Finch <jfinch@rawnet.com>
2017-04-07 09:22:56 +0100

    Will work for over 100 tweets

--- TwitterCompetitionWinner.php
+++ TwitterCompetitionWinner.php
@@ -8,14 +8,23 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-$winningStrings = ['Winning String1 ', 'Winning string2'];
+$count = 100;
+$winningStrings = ['Winning string1', 'Winning string2'];
 $q = '@rawnet';
+$sinceId = '849918275781877760';
+$maxId = '949918275781877760'; // Set an arbitrarily high max id
 
-$statuses = $connection->get("search/tweets", ["count" => 100,"q" => $q]);
+$tweets = [];
+$noOfTweets = 0;
+do {
+    $statuses = $connection->get("search/tweets", ["count" => $count,"q" => $q, "since_id" => $sinceId, 'max_id' => $maxId]);
 
-$tweets = $statuses->statuses;
+    $countOfTweetsReturned = count($statuses->statuses);
+    $maxId = $statuses->statuses[$countOfTweetsReturned-1]->id;
+    $tweets = array_merge($tweets, $statuses->statuses);
+    $noOfTweets += $countOfTweetsReturned;
+} while ($countOfTweetsReturned === $count);
 
-$potentialWinners = [];
 foreach ($tweets as $tweet) {
 
     foreach ($winningStrings as $winningString) {
@@ -28,8 +37,14 @@ foreach ($tweets as $tweet) {
 }
 
 $potentialWinners = array_unique($potentialWinners);
+$noOfPotetenialWinners = count($potentialWinners);
 echo <<<EOL
 RAWNET PHP TWITTER TAKEOVER COMPETITION WINNER SELECTOR
+
+{$noOfTweets} tweets received.
+{$noOfPotetenialWinners} correct answers.
+
+And....
 
 EOL;
 if (empty($potentialWinners)) {
@@ -38,5 +53,5 @@ if (empty($potentialWinners)) {
 }
 $winner = $potentialWinners[mt_rand(0, count($potentialWinners) - 1)];
 echo "The winner is $winner".PHP_EOL;
-$winningTweet = $potentiallyWinningTweet[$winner][mt_rand(0, count($potentiallyWinningTweet[$winner]) - 1)]->text;
-echo "Winning tweet: $winningTweet";
+$winningTweet = $potentiallyWinningTweet[$winner][mt_rand(0, count($potentiallyWinningTweet[$winner]) - 1)];
+echo "Winning tweet: '$winningTweet->text' sent at $winningTweet->created_at";
